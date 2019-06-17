64282482a7dd0fa26b8638781bda9d4f882c9fb9
Wendel de Oliveira Silva <36209846+silvermandf@users.noreply.github.com>
2019-06-17 15:18:15 -0300

    Remoção dos botões alterar e editar
    Remoção dos botões editar e alterar da lista de produtos, quando o usuário não estiver logado.

--- produto-lista.php
+++ produto-lista.php
@@ -2,6 +2,15 @@
 <?php include("conecta.php"); ?>
 <?php include("banco-produto.php"); ?>
 <?php include("banco-categoria.php"); ?>
+<?php include("logica-usuario.php"); ?>
+
+<?php 
+	if(array_key_exists("removido", $_GET) && $_GET["removido"] == "true") :
+?>
+		<p class="alert-success"> Produto apagado com sucesso.</p>
+<?php		
+	endif 
+?> 
 
 <table class="table table-striped table-bordered">
 	<thead class="thead-dark">
@@ -16,24 +25,34 @@
 	</thead>
 	<?php
 		$produtos = listaProdutos($conexao);
-		sort($produtos);
-				
 		foreach($produtos as $produto) :
-			$usado = $produto['usado'] ? "Usado" : "Novo";		
+			$usado = $produto['usado'] ? "Usado" : "Novo";
+			
 	?>	
-			<tbody>
-				<tr>
-					<th scope="row"><?= $produto["id"] ?></th>
-					<td><?= $produto["nome"] ?></td>
-					<td><?= $produto["preco"] ?></td>
-					<td><?= substr($produto["descricao"], 0, 40); print("...") ?></td>
-					<td><?= $produto["categoria"] ?></td>
+		<tbody>
+			<tr>
+				<th scope="row"><?= $produto["id"] ?></th>
+				<td><?= $produto["nome"] ?></td>
+				<td><?= $produto["preco"] ?></td>
+				<td><?= substr($produto["descricao"], 0, 40); print("...") ?></td>
+				<td><?= $produto["categoria"] ?></td>
+				<td>
+					<input type="hidden" name="usado" value="true"> <?= $usado ?>
+				</td>
+				<?php if(usuarioEstaLogado()) { ?>
 					<td>
-						<input type="hidden" name="usado" value="true"> <?= $usado ?>
+						<a href="produto-altera-formulario.php?id=<?= $produto['id'] ?>" class="btn btn-primary">Alterar</a>
 					</td>
-					
-				</tr>
-			</tbody>
+				<?php } ?>
+				<?php if(usuarioEstaLogado()) { ?>
+					<td>
+						<form action="remove-produto.php" method="post">
+							<input type="hidden" name="id" value="<?= $produto['id'] ?>">
+							<button class="btn btn-danger">Remover</button>
+						</form> 
+					</td>
+				<?php } ?>
+			</tr>
 	<?php
 		endforeach
 	?>
